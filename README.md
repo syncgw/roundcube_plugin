04a07ced0d3f3ea413f2c710a9ffdae751e77a6b
totep42 <2334583+syncgw@users.noreply.github>
2023-09-27 19:21:49 +0200

    9.19.78

---
+++ README.md
@@ -0,0 +1,86 @@
+# ![picture logo](https://github.com/syncgw/gui-bundle/blob/master/assets/syncgw.png "sync•gw") #
+ 
+![](https://img.shields.io/packagist/v/syncgw/roundcube-syncgw.svg)
+![](https://img.shields.io/packagist/l/syncgw/roundcube-syncgw.svg)
+![](https://img.shields.io/packagist/dt/syncgw/roundcube-syncgw.svg)
+ 
+**sync•gw** is the one and only fully portable server software available providing synchronization service between nearly any mobile device and your web server.
+
+## roundcube-syncgw bundle ##
+
+With this Plugin you can specify in your [RoundCube](https://roundcube.net) installation which address books, calendars, task lists and notes you want to synchronize with your cell phone / smart phone. For address boks you can specify whether you want to synchronize only contacts with a phone number specified or if you want to synchronize all contacts within this address book.
+
+**Requirements**
+
+To use this plugin, you need
+
+* A functional [RoundCube](https://roundcube.net) installation.
+* [sync•gw](https://github.com/syncgw) installed and configured in RoundCube root directory.
+
+**Installation**
+* Please install [sync•gw plugin](https://github.com/syncgw/roundcube-syncgw).
+
+   ```
+  composer require syncgw/roundcube-syncgw
+   ```
+
+* If you want to synchronize address books, then you don't need any additional RoundCube plugin.
+
+* If you want to use shared address books, then you need to install [globaladdressbook-Plugin](https://github.com/johndoh/roundcube-globaladdressbook).
+
+   ```
+   composer require johndoh/globaladdressbook
+   ```
+  
+* If you want to synchronize calendar, then you need to install [calender plugin](https://packagist.org/packages/kolab/calendar).
+
+   ```
+  composer require kolab/calendar
+   ```
+
+* If you want to synchronize tasklis, then you need to install [tasklist plugin](https://plugins.roundcube.net/packages/kolab/tasklist).
+
+   ```
+  composer require kolab/tasklist
+   ```
+  
+    **Caution:** If you use the plugin and receive a error message in RoundCube log file, then please check file `plugins/tasklist/config.inc.php`. There `$config['tasklist_driver'] = 'database';` should be specified.
+  
+* If you want to synchronize notes, then you need to install [ddnotes plugin](https://packagist.org/packages//dondominio/ddnotes).
+
+   ```
+  composer require dondominio/ddnotes 
+   ```
+
+* Activate our plugin by adding plugin name in file `config/config.inc.php`
+
+   ```
+  $config['plugins'] = array(
+	...
+	'syncgw_roundcube',
+	[the other optional plugins]
+	...
+  );
+   ```
+	
+**Usage**
+
+* Go to menu `Settings` and configure synchronization settings by selecting `Synchronization settings`.If this selection does not appear, then you did not install **sync•gw** in RoundCube root directory.
+* Now you're ready to synchronize your selected data with your cell phone / smart phone. If you need some help how to configure you device, take a look a [sync•gw FAQ](https://github.com/toteph42/syncgw/blob/master/downloads/FAQ.md).
+
+Please enjoy!
+
+If you enjoy my software, I would be happy to receive a donation.
+
+<a href="https://www.paypal.com/donate/?hosted_button_id=DS6VK49NAFHEQ" target="_blank" rel="noopener">
+  <img src="https://www.paypalobjects.com/en_US/DK/i/btn/btn_donateCC_LG.gif" alt="Donate with PayPal"/>
+</a>
+
+
+[[Documentation](https://github.com/syncgw/doc-bundle/blob/master/README.md)]
+[[System requirements](https://github.com/syncgw/doc-bundle/blob/master/PreReqs.md)] 
+[[Available bundles](https://github.com/syncgw/doc-bundle/blob/master/Packages.md)] 
+[[List of all changes](https://github.com/syncgw/doc-bundle/blob/master/Changes.md)] 
+[[Additional Downloads](https://github.com/syncgw/doc-bundle/blob/master/Downloads.md)] 
+[[Frequently asked questions](https://github.com/syncgw/doc-bundle/blob/master/FAQ.md)] 
+[[Supported feature](https://github.com/syncgw/doc-bundle/blob/master/Features.md)]
---
+++ roundcube-syncgw.php
@@ -0,0 +1,304 @@
+<?php
+
+use syncgw\lib\DataStore;
+use syncgw\lib\Config;
+
+/*
+ * sync*gw RoundCube Bundle
+ *
+ *	@copyright	(c) 2008 - 2023 Florian Daeumling, Germany. All right reserved
+ * 	@license 	LGPL-3.0-or-later
+ */
+
+class roundcube_syncgw extends rcube_plugin {
+
+	// full e-mail
+	const MAIL_FULL   = 'M';
+	// merge address books
+	const ABOOK_MERGE = 'X';
+	// full address book
+	const ABOOK_FULL  = 'A';
+	// only contacts with tlephone number assigned
+	const ABOOK_SMALL = 'P';
+	// full calendar
+	const CAL_FULL    = 'C';
+	// full task list
+	const TASK_FULL   = 'T';
+	// full notes
+	const NOTES_FULL  = 'N';
+	// max. number to check
+    const MAX         = 99;
+
+    public  $task     = 'settings';
+	private $cal_db   = null;
+	private $tsk_db   = null;
+	private $rc;
+
+	/**
+	 *	Initialize plugin
+	 * 	@see rcube_plugin::init()
+	 */
+	function init() {
+        $this->add_texts('localization/', false);
+
+		$this->include_stylesheet($this->local_skin_path().'/plugin.css');
+
+        $this->add_hook('preferences_sections_list', [ $this, 'select_for_sync_section' ]);
+        $this->add_hook('preferences_list',          [ $this, 'select_for_sync_pref' ]);
+		$this->add_hook('preferences_save',          [ $this, 'select_for_sync_save_pref' ]);
+
+		$this->rc = rcmail::get_instance();
+ 	}
+
+  	/**
+ 	 *  Add a section to the preference tab
+ 	 *
+	 *	@param 	array $args: section list
+	 *	@return array $args
+ 	 */
+    public function select_for_sync_section($args) {
+
+        $this->add_texts('localization/', false);
+
+        $args['list']['select_for_sync_pref'] = [
+                      'id'      => 'select_for_sync_pref',
+                      'section' => rcube::Q($this->gettext('syncgw_p_title')),
+        ];
+
+        return $args;
+    }
+
+	/**
+	 * 	Adds a checkbox selection list for synchronization
+	 *
+	 *	@param 	array $args: section list
+	 *	@return array $args
+	 */
+    public function select_for_sync_pref($args) {
+
+		if ($args['section'] != 'select_for_sync_pref')
+		    return $args;
+
+        $prefs = $this->rc->user->get_prefs();
+        $prefs = $prefs['syncgw'];
+
+        if (!file_exists($path = Config::getInstance()->getVar(Config::ROOT).'/core-bundle/assets/core/'.Config::CONFIG))
+   		 	return;
+
+        $enabled = @parse_ini_file($path);
+
+		if ($enabled['Datastores'] & DataStore::MAIL) {
+
+			$args['blocks']['syncgw_m']['name'] = $this->gettext('syncgw_m_head');
+        	$n = 0;
+        	$c = new html_checkbox([
+
+    				'name' 	    => '_syncgw_m'.$n,
+	   				'id' 	    => '_syncgw_m'.$n,
+					'value'     => self::MAIL_FULL.$n,
+        	]);
+    		$args['blocks']['syncgw_m']['options']['syncgw'.$n] = [
+
+	    	 	  'title' 	=> '"'.$this->gettext('syncgw_m_text').'"',
+			      'content' 	=> $c->show(strpos($prefs, self::MAIL_FULL.$n.';') !== false ? self::MAIL_FULL.$n : null),
+			];
+        }
+
+       	if ($enabled['Datastores'] & DataStore::CONTACT) {
+
+       		$args['blocks']['syncgw_a']['name'] = $this->gettext('syncgw_a_head');
+
+	        $n = 0;
+	    	foreach ($this->rc->get_address_sources() as $a) {
+
+	            $c = new html_checkbox([
+
+				         'name'     => '_syncgw_a'.$n,
+	    				 'id' 	    => '_syncgw_a'.$n,
+		       			 'value'    => self::ABOOK_FULL.$a['id'],
+				]);
+	            $p = new html_checkbox([
+
+				         'name'     => '_syncgw_p'.$n,
+	    				 'id' 	    => '_syncgw_p'.$n,
+		       			 'value'    => self::ABOOK_SMALL.$a['id'],
+				]);
+	            $args['blocks']['syncgw_a']['options']['syncgw'.$n] = [
+
+		       			'title' 	=> rcube::Q('"'.$a['name'].'"'),
+				     	'content' 	=> $c->show(strpos($prefs, self::ABOOK_FULL.$a['id'].';') !== false ? self::ABOOK_FULL.$a['id'] : null).' '.
+	                                   rcube::Q($this->gettext('syncgw_a_tonly')).' '.
+	                                   $p->show(strpos($prefs, self::ABOOK_SMALL.$a['id'].';') !== false ? self::ABOOK_SMALL.$a['id'] : null),
+	            ];
+	            $n++;
+			}
+    	    $c = new html_checkbox([
+
+				'name'     => '_syncgw_m',
+    			'id' 	   => '_syncgw_m',
+		       	'value'    => self::ABOOK_MERGE,
+			]);
+    	    $args['blocks']['syncgw_a']['options']['syncgw'.$n] = [
+
+		    	'title' 	=> $this->gettext('syncgw_x_text'),
+				'content' 	=> '&nbsp;&nbsp;'.$c->show(strpos($prefs, self::ABOOK_MERGE) !== false ? self::ABOOK_MERGE : null),
+    	    ];
+       	}
+
+       	if ($enabled['Datastores'] & DataStore::CALENDAR) {
+
+       		$args['blocks']['syncgw_c']['name'] = $this->gettext('syncgw_c_head');
+
+	        if (!is_object($this->cal_db)) {
+
+	            // calendar.php:function load_driver() -- START
+
+	        	if ($cal = $this->rc->plugins->get_plugin('calendar')) {
+
+		            $n = $this->rc->config->get('calendar_driver', 'database');
+	    	        $c = $n.'_driver';
+
+	        	    require_once($cal->home.'/drivers/calendar_driver.php');
+	            	require_once($cal->home.'/drivers/'.$n.'/'.$c.'.php');
+
+		            $this->cal_db = new $c($cal);
+	    	        // -- END
+	        	}
+	        }
+
+			$n = 0;
+			if ($this->cal_db) {
+
+		        foreach ($this->cal_db->list_calendars(calendar_driver::FILTER_ALL) as $a) {
+
+	    	        if (!$a['active'] || $a['id'] == calendar_driver::BIRTHDAY_CALENDAR_ID)
+	        	        continue;
+
+		            $c = new html_checkbox([
+    						'name' 	    => '_syncgw_c'.$n,
+		   			    	'id' 	    => '_syncgw_c'.$n,
+						    'value'     => self::CAL_FULL.$a['id'],
+		            ]);
+		    	    $args['blocks']['syncgw_c']['options']['syncgw'.$n] = [
+
+			       			'title' 	=> rcube::Q('"'.$a['name'].'"'),
+					     	'content' 	=> '&nbsp;&nbsp;'.$c->show(strpos($prefs, self::CAL_FULL.$a['id'].';') !== false ? self::CAL_FULL.$a['id'] : null),
+					];
+		    	    $n++;
+		        }
+			}
+       	}
+
+       	if ($enabled['Datastores'] & DataStore::TASK) {
+
+       		$args['blocks']['syncgw_t']['name'] = $this->gettext('syncgw_t_head');
+
+	        if (!is_object($this->tsk_db)) {
+
+	            // tasklist.php:function load_driver() -- START
+
+	        	if ($tsk = $this->rc->plugins->get_plugin('tasklist')) {
+
+		            $n   = $this->rc->config->get('tasklist_driver', 'database');
+	    	        $c   = 'tasklist_' . $n . '_driver';
+
+    	    	    require_once($tsk->home.'/drivers/tasklist_driver.php');
+    	        	require_once($tsk->home.'/drivers/'.$n.'/'.$c.'.php');
+
+		            $this->tsk_db = new $c($tsk);
+    		        // -- END
+    	    	}
+    	    }
+
+ 			$n = 0;
+ 			if ($this->tsk_db) {
+		        foreach ($this->tsk_db->get_lists(tasklist_driver::FILTER_ALL) as $a) {
+
+	    	        if (!$a['active'])
+	    	            continue;
+
+	    	        $c = new html_checkbox([
+	    					'name' 	    => '_syncgw_t'.$n,
+			   			    'id' 	    => '_syncgw_t'.$n,
+						    'value'     => self::TASK_FULL.$a['tasklist_id'],
+	    	        ]);
+	    		    $args['blocks']['syncgw_t']['options']['syncgw'.$n] = [
+			       			'title' 	=> rcube::Q('"'.$a['name'].'"'),
+					     	'content' 	=> '&nbsp;&nbsp;'.$c->show(strpos($prefs, self::TASK_FULL.$a['tasklist_id'].';') !== false ?
+					     	                        self::TASK_FULL.$a['tasklist_id'] : null),
+					];
+	    		    $n++;
+	    	    }
+ 			}
+       	}
+
+       	if ($enabled['Datastores'] & DataStore::NOTE) {
+
+       		$args['blocks']['syncgw_n']['name'] = $this->gettext('syncgw_n_head');
+
+	        if ($this->rc->plugins->get_plugin('ddnotes')) {
+
+		 		$n = 0;
+	            $c = new html_checkbox([
+
+	    				'name' 	    => '_syncgw_n'.$n,
+		   			    'id' 	    => '_syncgw_n'.$n,
+					    'value'     => self::NOTES_FULL.$n,
+	            ]);
+	    	    $args['blocks']['syncgw_n']['options']['syncgw'.$n] = [
+
+		       			'title' 	=> '"'.$this->gettext('syncgw_n_text').'"',
+				     	'content' 	=> '&nbsp;&nbsp;'.$c->show(strpos($prefs, self::NOTES_FULL.$n.';') !== false ?
+				     	                        self::NOTES_FULL.$n : null),
+				];
+	    	    $n++;
+	 		}
+       	}
+
+ 		return $args;
+    }
+
+    /**
+     * 	Save preferences
+     *
+	 *	@param 	array $args: section list
+	 *	@return array $args
+     */
+	public function select_for_sync_save_pref($args) {
+
+		if ($args['section'] != 'select_for_sync_pref')
+		    return $args;
+
+        $this->rc->config->get('syncgw');
+
+        $prefs = '';
+
+        for ($n=0; $n < self::MAX; $n++)
+            if (isset($_POST['_syncgw_m'.$n]))
+                $prefs .= $_POST['_syncgw_m'.$n].';';
+
+        if (isset($_POST['_syncgw_m']))
+        	$prefs .= self::ABOOK_MERGE.';';
+
+        for ($n=0; $n < self::MAX; $n++)
+            if (isset($_POST['_syncgw_a'.$n]))
+                $prefs .= $_POST['_syncgw_a'.$n].';'.$_POST['_syncgw_p'.$n].';';
+
+        for ($n=0; $n < self::MAX; $n++)
+            if (isset($_POST['_syncgw_c'.$n]))
+                $prefs .= $_POST['_syncgw_c'.$n].';';
+
+        for ($n=0; $n < self::MAX; $n++)
+            if (isset($_POST['_syncgw_t'.$n]))
+                $prefs .= $_POST['_syncgw_t'.$n].';';
+
+        for ($n=0; $n < self::MAX; $n++)
+            if (isset($_POST['_syncgw_n'.$n]))
+                $prefs .= $_POST['_syncgw_n'.$n].';';
+
+        $this->rc->user->save_prefs([ 'syncgw' => $prefs ]);
+
+        return $args;
+    }
+
+}
